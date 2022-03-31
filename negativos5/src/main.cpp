7158e994b1672f4db0835e3003c0a3faf6a7e719
HappyHoland <91347304+HappyHoland@users.noreply.github.com>
2022-03-31 18:06:14 -0300

    Submitting answer

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,18 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int nums[SIZE];
+    int cont = 0;
+
+    for (int i = 0; i < SIZE, i++) {
+        cin >> nums[i];
+
+        if(nums[i] < 0) {
+            cont++;
+        }
+    }
+
+    cout << cont << endl;
 
     return 0;
 }
